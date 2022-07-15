76da0e2f154272492dc195c1dfb9ed0ee1c368ff
hemant15kumar <hemant.kumar@andromeda360.ai>
2022-07-15 18:49:38 +0530

    15thjuly 1st cmd

--- test1.sh
+++ test1.sh
@@ -1,8 +1,11 @@
 working_branch=$1
 main_branch=$2
-commit_id=$3
+#commit_id=$(git log --format="%h" --after="1 day"| tail -1
+commit_id=$(git log --after="yesterday" --format="%h" | tail -1)
 git_url=https://github.com/andromeda360/a360-flux.git
 status=$(kubectl get pods --all-namespaces | grep 'a360\|auth\|istio-system' | grep -v 'auth-jwt' | grep -v 'Running' | wc -l)
+cat $commit_id
+cat $status
 if [ $status = 0 ]
 then
    echo "All Pods of all namespaces are up and running"
@@ -15,7 +18,7 @@ else
    else
    echo "Any of the Pods are not running,Kindly revert the cahnges"
    ##git log --oneline --graph
-   git log --after="yesterday"
+   ##git log --after="yesterday"
    git revert -m 1 $commit_id
    git push --set-upstream origin $working_branch
    gh pr create --title "PR has been created at $date as per previous Prod Running Sucessfully" --body "Reverted to previous woking state" --base $main_branch --head working_branch
